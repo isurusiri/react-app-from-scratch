a34524b9abd8a635f093cde4150a7c4326812f95
isurusiri <isuruanatomy@gmail.com>
2019-01-07 12:40:04 +0530

    added prettier and eslint

--- src/App.js
+++ src/App.js
@@ -1,36 +1,40 @@
-const Pet = (props) => {
-    return React.createElement("div", {}, [
-        React.createElement("h1", {}, props.name),
-        React.createElement("h2", {}, props.animal),
-        React.createElement("h3", {}, props.breed)
-    ]);
-}
+const Pet = props => {
+  return React.createElement("div", {}, [
+    React.createElement("h1", {}, props.name),
+    React.createElement("h2", {}, props.animal),
+    React.createElement("h3", {}, props.breed)
+  ]);
+};
 
 class App extends React.Component {
-    handleTitleClick() {
-        alert("You clicked the title");
-    }
-    
-    render() {
-        return React.createElement("div", {}, [
-            React.createElement("h1", { onClick: this.handleTitleClick }, "Adopt me!"),
-            React.createElement(Pet, {
-                name: "Luna",
-                animal: "dog",
-                breed: "Havanese"
-            }),
-            React.createElement(Pet, {
-                name: "Pepper",
-                animal: "bird",
-                breed: "Cockatiel"
-            }),
-            React.createElement(Pet, {
-                name: "Doink",
-                animal: "cat",
-                breed: "Mixed"
-            })
-        ]);
-    }
+  handleTitleClick() {
+    alert("You clicked the title");
+  }
+
+  render() {
+    return React.createElement("div", {}, [
+      React.createElement(
+        "h1",
+        { onClick: this.handleTitleClick },
+        "Adopt me!"
+      ),
+      React.createElement(Pet, {
+        name: "Luna",
+        animal: "dog",
+        breed: "Havanese"
+      }),
+      React.createElement(Pet, {
+        name: "Pepper",
+        animal: "bird",
+        breed: "Cockatiel"
+      }),
+      React.createElement(Pet, {
+        name: "Doink",
+        animal: "cat",
+        breed: "Mixed"
+      })
+    ]);
+  }
 }
 
 ReactDOM.render(React.createElement(App), document.getElementById("root"));
