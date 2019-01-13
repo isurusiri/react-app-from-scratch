1d483dbff3a7eb535f286bbb649dc3829f8cfcd5
isurusiri <isuruanatomy@gmail.com>
2019-01-13 21:58:39 +0530

    Made the carousel interactive

--- src/Carousel.js
+++ src/Carousel.js
@@ -16,6 +16,12 @@ class Carousel extends React.Component {
     return { photos };
   }
 
+  handleIndexClick = event => {
+    this.setState({
+      active: +event.target.dataset.index
+    });
+  };
+
   render() {
     const { photos, active } = this.state;
     return (
@@ -23,8 +29,12 @@ class Carousel extends React.Component {
         <img src={photos[active].value} alt="primary animal" />
         <div className="carousel-smaller">
           {photos.map((photo, index) => (
+            // img should be a button.
+            // eslint-disable-next-line
             <img
+              onClick={this.handleIndexClick}
               key={photo.value}
+              data-index={index}
               src={photo.value}
               className={index === active ? "active" : ""}
               alt="animal thumbnail"
