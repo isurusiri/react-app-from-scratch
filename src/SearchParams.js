2c5952d6487396737cbd1a32f159b163e6cef627
isurusiri <isuruanatomy@gmail.com>
2019-01-14 11:28:55 +0530

    bind api data to breed select

--- src/SearchParams.js
+++ src/SearchParams.js
@@ -1,11 +1,17 @@
 import React from "react";
-import { ANIMALS } from "petfinder-client";
+import pf, { ANIMALS } from "petfinder-client";
+
+const petfinderClient = pf({
+  key: process.env.API_KEY,
+  secret: process.env.API_SECRET
+});
 
 class SearchParams extends React.Component {
   state = {
     location: "Seattle, WA",
     animal: "",
-    bread: ""
+    breed: "",
+    breeds: []
   };
 
   handleLocationTextChange = event => {
@@ -15,10 +21,37 @@ class SearchParams extends React.Component {
   };
 
   handleAnimalTextChange = event => {
+    this.setState(
+      {
+        animal: event.target.value,
+        breed: ""
+      },
+      this.getBreeds
+    );
+  };
+
+  handleBreedTextChange = event => {
     this.setState({
-      animal: event.target.value
+      breed: event.target.value
     });
   };
+
+  getBreeds() {
+    if (this.state.animal) {
+      petfinderClient.breed.list({ animal: this.state.animal }).then(data => {
+        if (
+          data.petfinder &&
+          data.petfinder.breeds &&
+          data.petfinder.breeds.breed &&
+          Array.isArray(data.petfinder.breeds.breed)
+        ) {
+          this.setState({ breeds: data.petfinder.breeds.breed });
+        }
+      });
+    } else {
+      this.setState({ breeds: [] });
+    }
+  }
 
   render() {
     return (
@@ -48,6 +81,24 @@ class SearchParams extends React.Component {
             ))}
           </select>
         </label>
+        <label htmlFor="breed">
+          Breed
+          <select
+            id="breed"
+            value={this.state.breed}
+            onChange={this.handleBreedTextChange}
+            onBlur={this.handleBreedTextChange}
+            disabled={this.state.breeds.length === 0}
+          >
+            <option />
+            {this.state.breeds.map(breed => (
+              <option key={breed} value={breed}>
+                {breed}
+              </option>
+            ))}
+          </select>
+        </label>
+        <button>Search</button>
       </div>
     );
   }
