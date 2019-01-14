41e9b9bb7b9d00c0fba100bfcde5b522f4cccbe8
isurusiri <isuruanatomy@gmail.com>
2019-01-14 10:58:51 +0530

    added search params component

---
+++ src/SearchParams.js
@@ -0,0 +1,56 @@
+import React from "react";
+import { ANIMALS } from "petfinder-client";
+
+class SearchParams extends React.Component {
+  state = {
+    location: "Seattle, WA",
+    animal: "",
+    bread: ""
+  };
+
+  handleLocationTextChange = event => {
+    this.setState({
+      location: event.target.value
+    });
+  };
+
+  handleAnimalTextChange = event => {
+    this.setState({
+      animal: event.target.value
+    });
+  };
+
+  render() {
+    return (
+      <div className="search-params">
+        <label htmlFor="location">
+          Location
+          <input
+            id="location"
+            value={this.state.location}
+            placeholder="Location"
+            onChange={this.handleLocationTextChange}
+          />
+        </label>
+        <label htmlFor="animal">
+          Animal
+          <select
+            id="animal"
+            value={this.state.animal}
+            onChange={this.handleAnimalTextChange}
+            onBlur={this.handleAnimalTextChange}
+          >
+            <option />
+            {ANIMALS.map(animal => (
+              <option key={animal} value={animal}>
+                {animal}
+              </option>
+            ))}
+          </select>
+        </label>
+      </div>
+    );
+  }
+}
+
+export default SearchParams;
